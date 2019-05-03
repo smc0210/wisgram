d0baeb078a72da070fcfb3a7d63a7ef9d796cd79
smc <wiz@siot.do>
2019-05-03 11:07:46 +0900

    Setting up the Proejct, GraphQL Server and prosess

---
+++ src/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
+
+const PORT = process.env.PORT || 4000;
+
+const server = new GraphQLServer({ schema });
+
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () =>
+    console.log(`✅  Server running on  http://localhost:${PORT}`)
+);
