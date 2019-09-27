b966bb1a0946edfabc59c8d37c8edf02f6917c74
smc0210 <smc0210@naver.com>
2019-09-27 19:07:23 +0900

    모델 수정....prisma 싫다

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,17 +1,28 @@
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-    Mutation: {
-        createAccount: async (_, args) => {
-            const { username, email, firstName = "", lastName = "", bio = "" } = args;
-            const user = await prisma.createUser({
-                username,
-                email,
-                firstName,
-                lastName,
-                bio
-            });
-            return user;
-        }
-    } 
+  Mutation: {
+    createAccount: async (_, args) => {
+      const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const exists = await prisma.$exists.user({
+        OR: [
+          {
+            username
+          },
+          { email }
+        ]
+      });
+      if (exists) {
+        throw Error("이미 존재하는 username 이나 email 입니다.");
+      }
+      await prisma.createUser({
+        username,
+        email,
+        firstName,
+        lastName,
+        bio
+      });
+      return true;
+    }
+  }
 };
--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -6,12 +6,7 @@ export default {
     me: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      const userProfile = await prisma.user({ id: user.id });
-      const posts = await prisma.user({ id: user.id }).posts();
-      return {
-        user: userProfile,
-        posts
-      };
+      return await prisma.user({ id: user.id });
     }
   }
 };
